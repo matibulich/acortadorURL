fd572836eca0b1446cd3d88dd7452235e6bd53b7
matibulich <>
2024-07-19 12:13:06 -0300

    implementacion Mongo

--- index.js
+++ index.js
@@ -1,44 +1,64 @@
-const express = require("express")
-const app = express()
-const path = require("path")
-const shortId = require("shortid");
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const shortUrl = require("./models/modelo");
+require('dotenv').config();
 
-const PORT = 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-//alamacenador de urls
-const urls = []
+// Conexión a MongoDB
+mongoose.connect(process.env.MONGODB_URI,  {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("Conectado a MongoDB");
+}).catch(err => {
+  console.error("Error al conectar a MongoDB", err);
+});
 
-//midlewares
-app.use(express.static(path.resolve(__dirname,"public")))
-app.use(express.urlencoded({extended: false}))
+// Middlewares
+app.use(express.static(path.resolve(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
 
-//generador de plantillas
-app.set("view engine", "ejs")
-// app.set('views', path.join(__dirname, './views')); Usar si cambio las vistas de carpeta
+// Generador de plantillas
+app.set("view engine", "ejs");
 
-//rutas
+// Rutas
+app.get('/', async (req, res) => {
+  try {
+    const shortUrls = await shortUrl.find();
+    res.render('index', { shortUrls: shortUrls });
+  } catch (err) {
+    console.error("Error al obtener las URLs cortas:", err);
+    res.status(500).send("Error al obtener las URLs cortas.");
+  }
+});
 
-app.get('/', (req, res) => {
-    res.render('index', { shortUrl: urls });
-  });
+app.post("/acortadorurl", async (req, res) => {
+  try {
+    await shortUrl.create({ full: req.body.fullUrl });
+    res.redirect("/");
+  } catch (err) {
+    console.error("Error al acortar la URL:", err);
+    res.status(500).send("Error al acortar la URL.");
+  }
+});
 
-app.post("/acortadorurl", (req, res)=>{
-    const fullUrl = req.body.url;
-    const shortUrl = shortId.generate(8)
+app.get("/:shortUrl", async (req, res) => {
+  try {
+    const urlData = await shortUrl.findOne({ short: req.params.shortUrl });
+    if (urlData) {
+      res.redirect(urlData.full);
+    } else {
+      res.status(404).send("URL no encontrada.");
+    }
+  } catch (err) {
+    console.error("Error al redirigir la URL corta:", err);
+    res.status(500).send("Error al redirigir la URL corta.");
+  }
+});
 
-    urls.push({full: fullUrl, short: shortUrl })
-
-    res.redirect("/")
-} )
-
-app.get("/:shortUrl", async (req, res)=>{
-    const shortUrl = req.params.shortUrl;
-    const urlData = urls.find(e=> e.short === shortUrl)
-    res.redirect(urlData.full);
-})
-
-
-
-app.listen(PORT, ()=>{
-    console.log(`Servidor inicializado en http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Servidor inicializado en http://localhost:${PORT}`);
+});
